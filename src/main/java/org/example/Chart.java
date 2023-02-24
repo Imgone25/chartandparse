240fe7fcdfe4908364d37895d638d30143af5b21
Imgone25 <Imgone25@gmail.com>
2023-02-24 15:33:33 -0600

    Working bar chart and YAML parsing

--- src/main/java/org/example/Chart.java
+++ src/main/java/org/example/Chart.java
@@ -1,33 +1,69 @@
 package org.example;
 
 import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
+import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Chart {
     public static void main(String[] args) {
 
-        double[] yData = new double[] { 2.0, 1.0, 0.0 };
 
-        // Create Chart
-        XYChart chart = new XYChart(500, 400);
-        chart.setTitle("Sample Chart");
-        chart.setXAxisTitle("X");
-        chart.setXAxisTitle("Y");
-        XYSeries series = chart.addSeries("y(x)", null, yData);
-        series.setMarker(SeriesMarkers.CIRCLE);
-
-
+        CategoryChart test1 = getChart();
         try {
-            BitmapEncoder.saveBitmap(chart, "./Sample_Chart", BitmapEncoder.BitmapFormat.PNG);
-            BitmapEncoder.saveBitmap(chart, "./Sample_Chart", BitmapEncoder.BitmapFormat.JPG);
+            // save chart object to file
+            BitmapEncoder.saveBitmap(test1, "./Sample_Chart", BitmapEncoder.BitmapFormat.PNG);
 
-            VectorGraphicsEncoder.saveVectorGraphic(chart, "./Sample_Chart", VectorGraphicsEncoder.VectorGraphicsFormat.SVG);
+            // I don't think iText v5 supports SVG. This is the ideal format for PDF scaling.
+            VectorGraphicsEncoder.saveVectorGraphic(test1, "./Sample_Chart", VectorGraphicsEncoder.VectorGraphicsFormat.SVG);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
 
     }
+
+    public static CategoryChart getChart() {
+
+        // Create Chart
+        CategoryChart chart = new CategoryChartBuilder().width(800).height(800).title("Findings Release History").xAxisTitle("Version").yAxisTitle("Finding Count").build();
+
+        // Customize Chart
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+        chart.getStyler().setOverlapped(true);
+        chart.getStyler().setSeriesColors(new Color[] { new Color(114,114,114), new Color(3,111,252)});
+
+        String[] softwareVersions = { "Version 1.0", "Version 1.1", "Version 2.0", "Version 3.0" };
+        Integer[] unresolvedFindingsArray = { 230, 215, 158, 88 };
+        Integer[] newFindingsArray = { 0, 5, 3, 35 };
+        // sum the unresolved and new findings to show total on bar chart
+        for(int i = 0; i < newFindingsArray.length; ++i){
+            newFindingsArray[i] = newFindingsArray[i] + unresolvedFindingsArray[i];
+        }
+        // Series
+
+        chart.addSeries("New", Arrays.asList(softwareVersions), Arrays.asList(newFindingsArray));
+        // Unresolved is added second so overlap can be seen
+        chart.addSeries("Unresolved", Arrays.asList(softwareVersions), Arrays.asList(unresolvedFindingsArray));
+
+        // manually set Y-Axis so Legend doesn't cut off bar chart
+        int maxYValue = getMaxInteger(unresolvedFindingsArray, newFindingsArray); // getYAxisMax() returns null, must compare int arrays
+        chart.getStyler().setYAxisMax(maxYValue*1.1);
+
+        return chart;
+    }
+
+    public static int getMaxInteger(Integer[] a, Integer[] b) {
+        int maxA = Collections.max(Arrays.asList(a));
+        int maxB = Collections.max(Arrays.asList(b));
+
+        return (maxA > maxB) ? maxA : maxB;
+    }
 }
+
--- src/main/java/org/example/YamlParse.java
+++ src/main/java/org/example/YamlParse.java
@@ -3,14 +3,18 @@ package org.example;
 import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Map;
 
 public class YamlParse {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        /*
+        By creating a simple YAML file with just key/pair values,
+        we can store it in an Object and then cast to a map.
+         */
         Object object = null;
         try {
             YamlReader reader = new YamlReader(new FileReader("./src/main/resources/test.yaml"));
@@ -19,7 +23,20 @@ public class YamlParse {
             throw new RuntimeException(e);
         }
         System.out.println(object);
-        Map map = (Map)object;
-//        System.out.println(map.get("phone numbers"));
+        Map map = (Map) object;
+        System.out.println(map.get("header_text"));
+
+        // convert hex string to color
+        String hexCode = (String) map.get("footer_color");
+
+        Color footerColor = hexStringToColor(hexCode);
+    }
+
+    private static Color hexStringToColor(String s) {
+        int red = Integer.valueOf(s.substring(0, 2), 16);
+        int green = Integer.valueOf(s.substring(2, 4), 16);
+        int blue = Integer.valueOf(s.substring(4, 6), 16);
+
+        return new Color(red, green, blue);
     }
 }
